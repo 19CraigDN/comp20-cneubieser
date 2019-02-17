2a6b55085c6052b8276c4f6da7259cca59b4b236
19CraigDN <flipflopperies@gmail.com>
2019-02-17 17:46:55 -0500

    Synced up even better and added styling

--- butwehavethepower/lyrics.js
+++ butwehavethepower/lyrics.js
@@ -7,7 +7,7 @@ var lyrics = ['Lisa: Come gather round, children,', "It's high time ye learned",
               'Lenny: Now do "Classical Gas!"',"Lisa: So we'll march day and night","Lisa and Homer: By the big cooling tower.",
               "Everyone: They have the plant","But we have the power.","So we'll march day and night","By the big cooling tower.",
               "They have the plant","But we have the power."];
-var times = [5.85, 7.95, 10.6, 12.8, 16.9, 18.85, 21.4, 23.15, 38.1, 40.5, 42.95, 44.85, 52.6, 72.9, 74.9, 77.5, 79.65,
+var times = [5.85, 7.95, 10.6, 12.8, 16.9, 18.85, 21.4, 23.15, 38.1, 40.5, 42.95, 44.85, 52.6, 72.6, 74.9, 77.5, 79.65,
              82.2, 83.8, 87.1, 89.0, 120];
 var count = 0;
 
